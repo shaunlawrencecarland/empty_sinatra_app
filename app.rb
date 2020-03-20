fcc0b645522641f562e1100f3d87178329e85ebf
Shaun Carland <shaun@Shauns-MacBook-Pro-2.local>
2020-03-20 05:48:49 -0400

    flesh out endpoint

--- app.rb
+++ app.rb
@@ -1,14 +1,32 @@
 require 'sinatra'
 require "sinatra/reloader"
 require "pry"
+require "event"
 
+=begin
+    params: {
+        @country_code
+        @group_size
+    },
+
+    url: /v1/risk?country_code=ca&group_size=5000
+=end
 get "/v1/risk" do
-    # params: 
-    # country name:
-    # group size
+    group_size = params["group_size"].to_i
+    country_code = params["country_code"]
 
-    # 1(1-c/p)^g //N
-    # 
+    # get country data
+    country_adapter = CountryAdapter.new(country_code)
 
+    # get the corona virus api data
+    coronavirus_adapter = CoronavirusAdapter.new(country_code)
 
+    event = Event.new(
+        population: country_adapter.get_population, 
+        carriers: coronavirus_adapter.get_carriers, 
+        group_size: group_size
+    )
+
+    content_type :json
+    { risk: event.risk }.to_json
 end
